48269786792e2b9a9a7848343c90768bf6e16adf
cbukuba <cbukuba@student.s19.be>
2021-09-19 20:27:46 +0000

    v_00

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.beµ>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/19 17:51:00 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/19 17:51:03 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	fac;
+	int	i;
+
+	i = 1;
+	fac = 1;
+	while (i <= nb)
+	{
+		fac *= i;
+		i ++;
+	}
+	return (fac);
+	if (nb >)
+}
+#include <stdio.h>
+int main()
+{
+	printf("%d",ft_iterative_factorial(4));
+}

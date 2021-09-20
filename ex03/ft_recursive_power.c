307381a8b4b2711d971b23f112d2982ae46ad60b
cbukuba <cbukuba@student.s19.be>
2021-09-20 07:58:34 +0000

    Exercise03

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 07:29:06 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/20 07:29:11 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{	
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
+
+/*#include <stdio.h>
+int main()
+{
+	printf("%d",ft_recursive_power(4, 2));
+}*/

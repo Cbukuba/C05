a486a35858aa7c6035ba908746be0341be9fc843
cbukuba <cbukuba@student.s19.be>
2021-09-20 08:30:56 +0000

    Exercise04

---
+++ ex04/ft_fibonacci.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbukuba <cbukuba@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/20 07:59:32 by cbukuba           #+#    #+#             */
+/*   Updated: 2021/09/20 07:59:35 by cbukuba          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0 )
+		return (-1);
+	if (index == 1)
+		return (0);
+	if (index == 2)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+/*#include <stdio.h>
+int main ()
+{
+	printf("%d", ft_fibonacci(4));
+}*/

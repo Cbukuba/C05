33fb0284b555af1fb7a8dc8fb1200ebe0bf942f3
cbukuba <cbukuba@student.s19.be>
2021-09-21 21:30:16 +0000

    Exercise04fix

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -14,15 +14,15 @@ int	ft_fibonacci(int index)
 {
 	if (index < 0 )
 		return (-1);
-	if (index == 1)
+	if (index == 0)
 		return (0);
-	if (index == 2)
+	if (index == 2 || index == 1)
 		return (1);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
-
-/*#include <stdio.h>
-int main ()
-{
-	printf("%d", ft_fibonacci(4));
-}*/
+// 0 1 1 2 3 5 8 13 
+// #include <stdio.h>
+// int main ()
+// {
+// 	printf("%d", ft_fibonacci(4));
+// }
